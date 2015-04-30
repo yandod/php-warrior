6cd4e70daf4d88b94b1e55ac8c98d14550571c8c
Yusuke Ando <yandod@gmail.com>
2015-05-01 00:39:53 +0900

    fix exceeding max health

--- src/PHPWarrior/Abilities/Rest.php
+++ src/PHPWarrior/Abilities/Rest.php
@@ -11,7 +11,7 @@ class Rest extends Base {
     if ($this->unit->health() < $this->unit->max_health()) {
       $amount = round($this->unit->max_health()*0.1);
       if ( ($this->unit->health + $amount) > $this->unit->max_health() ) {
-        $amount = $this->unit->max_health() - $unit->health;
+        $amount = $this->unit->max_health() - $this->unit->health;
       }
       $this->unit->health += $amount;
       $this->unit->say("receives {$amount} health from resting, up to {$this->unit->health()} health");
