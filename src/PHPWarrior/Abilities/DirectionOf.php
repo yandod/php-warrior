13c6d447dff0db0c8f9b2700d975601666e02615
Yusuke Ando <yandod@gmail.com>
2015-05-03 17:19:57 +0900

    finish level4 and level5

---
+++ src/PHPWarrior/Abilities/DirectionOf.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace PHPWarrior\Abilities;
+
+class DirectionOf extends Base {
+
+  public $is_sense = true;
+
+  public function description() {
+    return ("Pass a Space as an argument, and the direction (:left, :right, :forward, :backward) to that space will be returned.");
+  }
+
+  public function perform($space) {
+    return $this->unit->position->relative_direction_of($space);
+  }
+}
---
+++ src/PHPWarrior/Abilities/Listen.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace PHPWarrior\Abilities;
+
+
+class Listen extends Base {
+
+  public $is_sense = true;
+
+  public function description() {
+    return "Returns an array of all spaces which have units in them.";
+  }
+
+  public function perform() {
+    $map = [];
+    foreach ($this->unit->position->floor->units() as $unit) {
+      if ($unit !== $this->unit) {
+        $map[] = $unit->position->space();
+      }
+    }
+    return $map;
+  }
+
+}
