03073bd4f0e1881172de13efc15900f6e0be23c1
Yusuke Ando <yandod@gmail.com>
2015-06-19 23:44:49 +0900

    doing

--- src/PHPWarrior/Abilities/Walk.php
+++ src/PHPWarrior/Abilities/Walk.php
@@ -5,21 +5,24 @@ namespace PHPWarrior\Abilities;
 class Walk extends Base {
 
   public function description() {
-    return "Move in the given direction (forward by default).";
+    return __("Move in the given direction (forward by default).");
   }
 
   public function perform($direction = ':forward') {
     $this->verify_direction($direction);
     if ($this->unit->position) {
       $s_direction = str_replace(':','',$direction);
-      $this->unit->say("walks {$s_direction}");
+      $this->unit->say(sprintf(__("walks %s"),$s_direction));
       if ($this->space($direction)->is_empty()) {
         call_user_func_array(
           [$this->unit->position,'move'],
           $this->offset($direction)
         );
       } else {
-        $this->unit->say("bumps into ". $this->space($direction));
+        $this->unit->say(sprintf(
+          __("bumps into %s"),
+          $this->space($direction)
+        ));
       }
     }
   }
--- templates/README.php
+++ templates/README.php
@@ -1,17 +1,17 @@
-<?php __('Level'); ?> <?= $this->level->number ?>
+<?php echo __('Level'); ?> <?= $this->level->number ?>
 
 
 <?= $this->level->description ?>
 
 
-<?php __('Tip'); ?>: <?= $this->level->tip ?>
+<?php echo __('Tip'); ?>: <?= $this->level->tip ?>
 
 
 <?= $this->level->floor->character() ?>
 
   > = <?php echo __('Stairs'); ?>
 <?php foreach ($this->level->floor->unique_units() as $unit): ?>
-  <?= $unit->character(); ?> = <?= $unit->name(); ?> (<?= $unit->max_health() ?> HP)
+  <?= $unit->character(); ?> = <?= $unit->name(); ?> (<?= $unit->max_health() ?> HP) 
 <?php endforeach; ?>
 
 <?php echo __('Warrior Abilities');?>:
